17d8646d3073efa6b79d65dcb0df5222fed84dc2
Harshit Shukla <mrharshit2409@gmail.com>
2024-04-07 11:39:27 +0530

    added monaco code editor

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
-import HomePage from "./Components/HomePage"
+import { Box } from "@chakra-ui/react";
+import CodeEditor from "./Components/CodeEditor";
 
 function App() {
   return (
-    <HomePage />
-  )
+    <Box minH="100vh" bg="#0f0a19" color="gray.500" px={6} py={8}>
+      <CodeEditor />
+    </Box>
+  );
 }
 
-export default App
+export default App;
