5f2772110a0ba2b79d66dd9f8334ae78f2e7334e
Harshit Shukla <mrharshit2409@gmail.com>
2024-04-08 14:34:27 +0530

    made some ui changes

--- src/Components/CodeEditor.jsx
+++ src/Components/CodeEditor.jsx
@@ -1,57 +1,95 @@
-import React, { useState,useRef } from 'react'
-import { Link } from 'react-router-dom'
-import { Box,HStack } from '@chakra-ui/react'
-import {Editor} from '@monaco-editor/react'
+import React, { useState, useRef } from 'react';
+import { Link } from 'react-router-dom';
+import { Box, Button, Text, useToast } from '@chakra-ui/react';
+import { Editor } from '@monaco-editor/react';
 import LanguageSelector from './LanguageSelector';
 import { CODE_SNIPPETS } from '../langsupport';
-import logo from '../assets/logo.svg'
-import './css/CodeEditor.css'
-import OutputPanel from './OutputPanel';
+import logo from '../assets/logo.svg';
+import './css/CodeEditor.css';
+import { executeCode } from '../api';
 
 const CodeEditor = () => {
-
     const editorRef = useRef();
-    const [value, setvalue] = useState("");
-    const [language, setlanguage] = useState("javascript");
-    
+    const toast = useToast();
+
+    const [value, setValue] = useState("");
+    const [language, setLanguage] = useState("javascript");
+    const [output, setOutput] = useState(null);
+    const [isLoading, setIsLoading] = useState(false);
+    const [isError, setIsError] = useState(false);
+    const [hasRun, setHasRun] = useState(false); // Track whether the code has been executed
+
     const onMount = (editor) => {
         editorRef.current = editor;
         editor.focus();
     };
 
     const onSelect = (language) => {
-        setlanguage(language);
-        setvalue(CODE_SNIPPETS[language])
-    }
+        setLanguage(language);
+        setValue(CODE_SNIPPETS[language]);
+    };
+
+    const runCode = async () => {
+        const sourceCode = editorRef.current.getValue();
+        if (!sourceCode) return;
+        try {
+            setIsLoading(true);
+            const { run: result } = await executeCode(language, sourceCode);
+            setOutput(result.output.split("\n"));
+            result.stderr ? setIsError(true) : setIsError(false);
+            setHasRun(true); // Update the state when code has been executed
+        } catch (error) {
+            toast({
+                title: "An error occurred.",
+                description: error.message || "Unable to run code",
+                status: "error",
+                duration: 6000,
+            });
+        } finally {
+            setIsLoading(false);
+        }
+    };
 
     return (
-        <Box minH="100vh" bg="#000" color="gray.500" px={6} py={8}>
-            <HStack spacing={4}>
-                <Box w="50%">
-
-                    <div className='logo-container'>
-                        <Link to='/' className='logo'>
-                            <img src={logo} alt="Logo" className="logo-img" />
-                            <h1 className="logo-text">CodeLogs</h1>
-                        </Link>
-                    </div>
-                
-                    <LanguageSelector language={language} onSelect={onSelect} />
-                    <Editor
-                        height="90vh" 
-                        theme='vs-dark'
-                        language={language}
-                        defaultValue={CODE_SNIPPETS[language]}
-                        onMount={onMount}
-                        value={value}
-                        onChange={(value) => setvalue(value)}
-                    />
+        <Box minH="100vh" bg="#000" color="gray.500" px={6} py={8} className='main-box'>
+            <div className='logo-container'>
+                <Link to='/' className='logo'>
+                    <img src={logo} alt="Logo" className="logo-img" />
+                    <h1 className="logo-text">CodeLogs</h1>
+                </Link>
+            </div>
+            <div className='buttons'>
+                <LanguageSelector language={language} onSelect={onSelect} />
+                <Button className='runcode-btn' variant='outline' colorScheme='green' isLoading={isLoading} onClick={runCode}>
+                    Run Code
+                </Button>
+            </div>
+            <Box className='editor-boxes'>
+                <Editor
+                    className={hasRun ? 'left' : 'left-side'} // Conditionally set the class name
+                    height="90vh"
+                    width="50%"
+                    theme='vs-dark'
+                    language={language}
+                    defaultValue={CODE_SNIPPETS[language]}
+                    onMount={onMount}
+                    value={value}
+                    onChange={(value) => setValue(value)}
+                />
+                <Box
+                    className={hasRun ? 'right' : 'right-side'} // Conditionally set the class name
+                    height="90vh"
+                    p={2}
+                    color={isError ? "red.400" : ""}
+                    border="1px solid"
+                    borderRadius={4}
+                    borderColor={isError ? "red.500" : "#333"}
+                >
+                    {output ? output.map((line, i) => <Text key={i}>{line}</Text>) : 'Click "Run Code" to see the output here'}
                 </Box>
-                <OutputPanel editorRef={editorRef} language={language} />
-            </HStack>
-            
+            </Box>
         </Box>
-    )
-}
+    );
+};
 
-export default CodeEditor
+export default CodeEditor;
